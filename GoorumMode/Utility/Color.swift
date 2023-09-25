6906d0ebb3c3ea8f94788c374f3f5ad9001f5983
sojin-p <luv2hong@gmail.com>
2023-09-25 20:54:44 +0900

    [Feat] 기본 Color Assets 추가

---
+++ GoorumMode/Utility/Color.swift
@@ -0,0 +1,33 @@
+//
+//  Color.swift
+//  GoorumMode
+//
+//  Created by 박소진 on 2023/09/25.
+//
+
+import UIKit
+
+extension Constants {
+    
+    enum Color {
+        
+        enum Text {
+            static let basicTitle = UIColor(named: "Basic_Title")
+            static let basicSubTitle = UIColor(named: "Basic_SubTitle")
+        }
+        
+        enum Background {
+            static let basic = UIColor(named: "Basic_Background")
+            
+            static let smiling = UIColor(named: "Smiling_Theme")
+            
+            static let Neutral = UIColor(named: "Neutral_Theme")
+        }
+        
+        enum Icon {
+            static let basic = UIColor(named: "Basic_Title")
+        }
+        
+    }
+    
+}
---
+++ GoorumMode/Utility/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GoorumMode
+//
+//  Created by 박소진 on 2023/09/25.
+//
+
+import UIKit
+
+enum Constants {
+
+    
+}
