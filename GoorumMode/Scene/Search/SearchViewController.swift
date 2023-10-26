cc76b8e6320a175b27aa6e8f857cdcecf12e804f
Sojin Park <luv2hong@gmail.com>
2023-10-26 15:59:38 +0900

    [Remove] 주석, 브레이크 포인트 삭제

--- GoorumMode/Scene/Search/SearchViewController.swift
+++ GoorumMode/Scene/Search/SearchViewController.swift
@@ -11,7 +11,6 @@ final class SearchViewController: BaseViewController {
     
     let searchBar = {
         let view = UISearchBar()
-//        view.searchTextField.font = Constants.Font.regular(size: 15)
         view.searchTextField.placeholder = "search_placeholder".localized
         view.setImage(UIImage(), for: UISearchBar.Icon.search, state: .normal)
         view.searchTextField.backgroundColor = UIColor.clear
