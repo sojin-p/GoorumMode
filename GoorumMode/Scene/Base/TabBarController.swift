7021c414c8c676c5dd2ceef86c84b1ae0df5373c
Sojin Park <luv2hong@gmail.com>
2023-11-24 21:29:07 +0900

    [Style] TabBarController 다국어 적용

--- GoorumMode/Scene/Base/TabBarController.swift
+++ GoorumMode/Scene/Base/TabBarController.swift
@@ -12,20 +12,20 @@ final class TabBarController: UITabBarController {
     private let moodVC = {
         let vc = MoodViewController()
         let nav = UINavigationController(rootViewController: vc)
-        vc.tabBarItem = UITabBarItem(title: "기록", image: Constants.IconImage.list, tag: 0)
+        vc.tabBarItem = UITabBarItem(title: "record".localized, image: Constants.IconImage.list, tag: 0)
         return nav
     }()
 
     private let chartVC = {
         let vc = ChartViewController()
-        vc.tabBarItem = UITabBarItem(title: "차트", image: Constants.IconImage.stats, tag: 1)
+        vc.tabBarItem = UITabBarItem(title: "chart".localized, image: Constants.IconImage.stats, tag: 1)
         return vc
     }()
 
     private let settingVC = {
         let vc = SettingViewController()
         let nav = UINavigationController(rootViewController: vc)
-        vc.tabBarItem = UITabBarItem(title: "설정", image: Constants.IconImage.setting, tag: 2)
+        vc.tabBarItem = UITabBarItem(title: "setting".localized, image: Constants.IconImage.setting, tag: 2)
         return nav
     }()
 
