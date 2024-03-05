5f36c62321eadad8851d2ae772ecaaae90acf8dc
Sojin Park <luv2hong@gmail.com>
2024-03-05 15:41:12 +0900

    [Add] NotiVC 추가

---
+++ GoorumMode/Scene/Setting/Notification/NotificationViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NotificationViewController.swift
+//  GoorumMode
+//
+//  Created by 박소진 on 2024/03/05.
+//
+
+import UIKit
+
+final class NotificationViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("NotificationViewController")
+    }
+    
+    override func configure() {
+        super.configure()
+    }
+    
+}
