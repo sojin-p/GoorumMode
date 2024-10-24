40bfcb6547d77d87806c125843d6389126f6a4bf
Sojin Park <luv2hong@gmail.com>
2024-10-24 20:47:47 +0900

    [Add] NavigationLazyView 추가

--- GoorumMode/Scene/Base/NavigationLazyView.swift
+++ GoorumMode/Scene/Base/NavigationLazyView.swift
@@ -7,12 +7,16 @@
 
 import SwiftUI
 
-struct NavigationLazyView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct NavigationLazyView<T: View>: View {
+    
+    let build: () -> T
+    
+    init(_ build: @autoclosure @escaping () -> T) {
+        self.build = build
     }
-}
-
-#Preview {
-    NavigationLazyView()
+    
+    var body: some View {
+        build()
+    }
+    
 }
