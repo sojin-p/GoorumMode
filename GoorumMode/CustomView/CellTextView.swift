7666c93c8b00f8799c6c741c25ecc9dc26fade20
Sojin Park <luv2hong@gmail.com>
2024-10-24 22:52:37 +0900

    [Add] SwiftUI용 CellStyle Text CustomView 추가

--- GoorumMode/CustomView/CellTextView.swift
+++ GoorumMode/CustomView/CellTextView.swift
@@ -5,4 +5,19 @@
 //  Created by 박소진 on 10/24/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct CellTextView: View {
+    
+    var title: String
+
+    var body: some View {
+        Text(title)
+            .font(Font.init(uiFont: Constants.Font.bold(size: 16)))
+            .foregroundColor(Color(uiColor: Constants.Color.Text.basicTitle))
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+            .contentShape(Rectangle())
+            .accessibilityLabel(title)
+    }
+    
+}
