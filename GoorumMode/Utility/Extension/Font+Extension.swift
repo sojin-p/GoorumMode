0060be97fc0d2f451161df42339da3669a2ae031
Sojin Park <luv2hong@gmail.com>
2024-10-24 20:25:17 +0900

    [Add] SwiftUI Font+Extension 추가

--- GoorumMode/Utility/Extension/Font+Extension.swift
+++ GoorumMode/Utility/Extension/Font+Extension.swift
@@ -1,8 +1,16 @@
 //
-//  Font+Extention.swift
+//  Font+Extension.swift
 //  GoorumMode
 //
 //  Created by 박소진 on 10/23/24.
 //
 
-import Foundation
+import SwiftUI
+
+extension Font {
+    
+    init(uiFont: UIFont) {
+        self = Font.custom(uiFont.fontName, size: uiFont.pointSize)
+    }
+    
+}
