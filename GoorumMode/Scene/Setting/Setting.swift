2437497467f47baf0cb491da371e14926ae2b0ff
Sojin Park <luv2hong@gmail.com>
2024-11-01 23:41:13 +0900

    [Refactor] Setting 모델에 접근성 코드 추가

--- GoorumMode/Scene/Setting/Setting.swift
+++ GoorumMode/Scene/Setting/Setting.swift
@@ -5,7 +5,6 @@
 //  Created by 박소진 on 2024/02/24.
 //
 
-import UIKit
 import Foundation
 
 //MARK: - UserSettingSection
@@ -28,30 +27,46 @@ enum SettingType {
     case none(isMail: Bool)
 }
 
-//MARK: - SettingUI
-struct SettingUI: Identifiable {
+//MARK: - Setting
+struct Setting: Identifiable {
     let id = UUID()
     let title: SettingTitle
     let iconName: String?
     let type: SettingType
+    let items: [Setting]? = nil
     
     enum SettingTitle: String {
+        //section1
         case noti = "setting_Noti"
         case notiTime = "setting_NotiTime"
         case info = "setting_Info"
         case inquiry = "setting_Inquiry"
         
+        //section2
         case version = "setting_Version"
         
-//        var accessibilityHint: String {
-//            switch self {
-//            case .noti: "알림을 설정하려면 스위치를 켜세요."
-//            case .notiTime: "알림 시간을 설정하려면 두 번 탭 하세요."
-//            case .info: "정책 등의 정보를 확인하려면 두 번 탭 하세요."
-//            case .inquiry: "개발자에게 문의 메일을 보내려면 두 번 탭 하세요 메일 화면이 켜집니다."
-//            case .version: "앱 버전 정보는 ~입니다."
-//            }
-//        }
+        var accessibilityHint: String {
+            switch self {
+            case .notiTime: "알림 시간을 설정하려면 두 번 탭 하세요."
+            case .info: "정책 등의 정보를 확인하려면 두 번 탭 하세요."
+            case .inquiry: "개발자에게 문의 메일을 보내려면 두 번 탭 하세요."
+            default: ""
+            }
+        }
+        
+        func localized() -> String {
+            return self.rawValue.localized
+        }
+    }
+    
+    enum Info: String {
+        case privacyPolicy = "setting_PrivacyPolicy"
+        
+        var accessibilityHint: String {
+            switch self {
+            case .privacyPolicy: "개인정보 정책을 확인하려면 두 번 탭 하세요."
+            }
+        }
         
         func localized() -> String {
             return self.rawValue.localized
