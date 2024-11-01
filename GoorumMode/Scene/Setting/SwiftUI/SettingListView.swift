1041b51a58fa628d420907ca47d695cc7a88dc6c
Sojin Park <luv2hong@gmail.com>
2024-11-01 23:43:22 +0900

    [Feat] 설정 화면 접근성 기능 및 코드 정리

--- GoorumMode/Scene/Setting/SwiftUI/SettingListView.swift
+++ GoorumMode/Scene/Setting/SwiftUI/SettingListView.swift
@@ -11,67 +11,31 @@ struct SettingList: View {
 
     @ObservedObject var viewModel: SettingUIViewModel
     
-    var item: SettingUI
+    var item: Setting
     
     var body: some View {
         
         HStack {
             
             if let iconName = item.iconName {
-                Image(uiImage: UIImage.templateImage(named: iconName) ?? UIImage())
-                    .accessibilityHidden(true)
+                iconView(iconName)
             }
             
             switch item.type {
                 
             case .toggle(_):
-                Toggle(
-                    item.title.localized(),
-                    isOn: $viewModel.isNotificationOn
-                )
-                .toggleStyle(BlackToggleStyle())
+                toggleView()
                 
             case .detailText(let value, let showPopUp):
-                CellTextView(title: item.title.localized())
-//                    .accessibilityHint(item.title.accessibilityHint)
-                Spacer()
-                if showPopUp {
-                    DatePicker("", selection: $viewModel.time, displayedComponents: .hourAndMinute)
-                        .labelsHidden()
-                        .onChange(of: viewModel.time) { newValue in
-                            if viewModel.isNotificationOn {
-                                viewModel.scheduleNotification(at: newValue)
-                                viewModel.showingPopup = true
-                            }
-                        }
-                } else {
-                    Text(value)
-                        .foregroundStyle(Color(uiColor: Constants.Color.Text.basicSubTitle))
-                        .font(Font.init(
-                            uiFont: Constants.Font.bold(size: showPopUp ? 16 : 13))
-                        )
-                }
+                detailTextView(value, showPopUp: showPopUp)
                 
             case .none(let isMail):
-                if isMail {
-                    CellTextView(title: item.title.localized())
-//                        .accessibilityHint(item.title.accessibilityHint)
-                        .onTapGesture {
-                            viewModel.sendEmail()
-                        }
-                } else {
-                    NavigationLink {
-                        NavigationLazyView(InfoView(title: item.title.localized()))
-                    } label: {
-                        CellTextView(title: item.title.localized())
-//                            .accessibilityHint(item.title.accessibilityHint)
-                    }
-
-                }
-                
+                noneView(isMail)
             }
             
         }
+        .accessibilityElement(children: .combine)
+        .accessibilityHint(item.title.accessibilityHint)
         .frame(height: 40)
         .padding(.leading, item.iconName == nil ? 6 : 0)
         .transition(.asymmetric(
@@ -79,4 +43,56 @@ struct SettingList: View {
             removal: .opacity))
         
     }
+    
+    private func iconView(_ iconName: String) -> some View {
+        Image(uiImage: UIImage.templateImage(named: iconName) ?? UIImage())
+    }
+    
+    private func toggleView() -> some View {
+        Toggle(
+            item.title.localized(),
+            isOn: $viewModel.isNotificationOn
+        )
+        .toggleStyle(BlackToggleStyle())
+    }
+    
+    private func detailTextView(_ value: String, showPopUp: Bool) -> some View {
+        HStack {
+            CellTextView(title: item.title.localized())
+            Spacer()
+            
+            if showPopUp {
+                DatePicker("", selection: $viewModel.time, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                    .onChange(of: viewModel.time) { newValue in
+                        if viewModel.isNotificationOn {
+                            viewModel.scheduleNotification(at: newValue)
+                            viewModel.showingPopup = true
+                        }
+                    }
+            } else {
+                Text(value)
+                    .foregroundStyle(Color(uiColor: Constants.Color.Text.basicSubTitle))
+                    .font(Font(uiFont: Constants.Font.bold(size: 13)))
+            }
+        }
+    }
+    
+    private func noneView(_ isMail: Bool) -> some View {
+        if isMail {
+            return AnyView(
+                CellTextView(title: item.title.localized())
+                    .onTapGesture {
+                        viewModel.sendEmail()
+                    }
+            )
+        } else {
+            return AnyView(
+                NavigationLink(destination: NavigationLazyView(InfoView(title: item.title.localized()))) {
+                    CellTextView(title: item.title.localized())
+                }
+            )
+        }
+    }
+    
 }
