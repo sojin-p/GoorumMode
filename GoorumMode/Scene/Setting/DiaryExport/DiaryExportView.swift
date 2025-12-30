bff947e571ce0993fe53f18ac5d535db6de83516
Sojin Park <luv2hong@gmail.com>
2025-12-30 19:54:16 +0900

    [Add] Assets에 Export Icon 이미지 추가

---
+++ GoorumMode/Scene/Setting/DiaryExport/DiaryExportView.swift
@@ -0,0 +1,18 @@
+//
+//  DiaryExportView.swift
+//  GoorumMode
+//
+//  Created by 박소진 on 12/30/25.
+//
+
+import SwiftUI
+
+struct DiaryExportView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiaryExportView()
+}
