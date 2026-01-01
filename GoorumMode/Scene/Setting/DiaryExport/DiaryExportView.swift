7ed242c5534aa61d827693cafa76d18295131093
Sojin Park <luv2hong@gmail.com>
2026-01-01 18:16:57 +0900

    [Add] DiaryExportView 파일 추가

--- GoorumMode/Scene/Setting/DiaryExport/DiaryExportView.swift
+++ GoorumMode/Scene/Setting/DiaryExport/DiaryExportView.swift
@@ -12,7 +12,7 @@ struct DiaryExportView: View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
-
-#Preview {
-    DiaryExportView()
-}
+//
+//#Preview {
+//    DiaryExportView()
+//}
